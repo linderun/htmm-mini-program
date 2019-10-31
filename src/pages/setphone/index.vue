677d004b20860218a25461be2ee63dd96865b01d
darren <darren@rantion.com>
2019-10-31 20:55:59 +0800

    设置手机号

--- src/pages/setphone/index.vue
+++ src/pages/setphone/index.vue
@@ -1,187 +1,195 @@
 <template>
-	<div class="loginbox">
-		<form>
-			
-			 <div class="group flex flex-align-center">
-				<i class="phone"><image src="/static/images/icon1.png" mode="aspectFit"/></i>
-				<input class="flex-1" type="text" value="" placeholder="请输入手机号码" v-model="phone" />
-			</div> 
-			<div class="clearfix codebox mt20">
-				<div class="group flex flex-align-center">
-					<i class="code">
-						<image src="/static/images/icon7.png" mode="aspectFit" /></i>
-					<input type="text" value="" placeholder="验证码" class="flex-1" v-model='code' />
-				</div>
+  <div class="loginbox">
+    <form>
 
-				<button class="getcode" :disabled="isAble=='0'" :class="{'active':isAble=='1'}" @click="getCode()">{{codetxt}}</button>
-			</div>
+      <div class="group flex flex-align-center">
+        <i class="phone">
+          <image src="/static/images/icon1.png" mode="aspectFit"/>
+        </i>
+        <input class="flex-1" type="text" value="" placeholder="请输入手机号码" v-model="phone"/>
+      </div>
+      <div class="clearfix codebox mt20">
+        <div class="group flex flex-align-center">
+          <i class="code">
+            <image src="/static/images/icon7.png" mode="aspectFit"/>
+          </i>
+          <input type="text" value="" placeholder="验证码" class="flex-1" v-model='code'/>
+        </div>
 
-			
-			<div class="submitBtn" @click="setPayPwd()">确定</div>
-		</form>
-	</div>
+        <button class="getcode" :disabled="isAble=='0'" :class="{'active':isAble=='1'}" @click="getCode()">{{codetxt}}
+        </button>
+      </div>
+
+
+      <div class="submitBtn" @click="setPayPwd()">确定</div>
+    </form>
+  </div>
 </template>
 
 <script>
-	export default {
-		components: {
+    export default {
+        components: {},
 
-		},
-
-		data() {
-			return {
-				code: '',
-				phone: '',
-				pwd: '',
-				confirmpwd: '',
-				isAble: '0',
-				codetxt: '获取验证码'
-			}
-		},
-		onShow() {
-			this.getUserInfo();
-		},
-		mounted() {
-			// 			this.phone = this.$root.$mp.query.phone;
-			// 			console.log(this.phone)
-		},
-		onUnload() {
-			this.code = '';
-			this.phone = '';
-			this.pwd = '';
-			this.confirmpwd = '';
-			this.isAble = '0';
-			this.codetxt = '获取验证码';
-		},
-		methods: {
-			
-			getCode() {
-				var that = this;
-				/* if (that.isAble == '1') {
-					that.isAble = '0';
-					var num = 60;
-					that.codetxt = num + 's';
-					var timer = setInterval(function() {
-						if (num == 1) {
-							clearInterval(timer);
-							that.codetxt = '获取验证码';
-							that.isAble = '1';
-						} else {
-							num--;
-							that.codetxt = num + 's';
-						}
-					}, 1000);
-					api.sendCode({
-						tel: that.phone,
-						action: 'setPayPasswd'
-					}).then(da => {
-						wx.showToast({
-							title: da.msg,
-							icon: 'none'
-						})
-					})
-				} */
-			},
-			setPayPwd() {
-				if (!regPhone(this.phone)) {
-					wx.showToast({
-						title: '请输入正确的手机号码',
-						icon: 'none'
-					})
-					return false;
-				}
-				
-			}
-		},
-		watch: {
-			
-		},
-		computed: {
-
-		}
-	}
+        data() {
+            return {
+                code: '',
+                phone: '',
+                pwd: '',
+                confirmpwd: '',
+                isAble: '0',
+                codetxt: '获取验证码'
+            }
+        },
+        onShow() {
+            this.getUserInfo();
+        },
+        mounted() {
+            // 			this.phone = this.$root.$mp.query.phone;
+            // 			console.log(this.phone)
+        },
+        onUnload() {
+            this.code = '';
+            this.phone = '';
+            this.pwd = '';
+            this.confirmpwd = '';
+            this.isAble = '0';
+            this.codetxt = '获取验证码';
+        },
+        methods: {
+            getCode() {
+                var that = this;
+                if (that.isAble == '1') {
+                    that.isAble = '0';
+                    var num = 60;
+                    that.codetxt = num + 's';
+                    var timer = setInterval(function () {
+                        if (num == 1) {
+                            clearInterval(timer);
+                            that.codetxt = '获取验证码';
+                            that.isAble = '1';
+                        } else {
+                            num--;
+                            that.codetxt = num + 's';
+                        }
+                    }, 1000);
+                    api.sendVerifyCode(that.phone).then(da => {
+                        var data = da.param;
+                        if (data.code == 200) {
+                            wx.showToast({
+                                title: '验证码已发送，请查看短信',
+                                icon: 'success'
+                            });
+                        } else {
+                            wx.showToast({
+                                title: '验证码发送失败，请稍后重试',
+                                icon: 'none'
+                            });
+                        }
+                    })
+                }
+            },
+            setPayPwd() {
+                if (!regPhone(this.phone)) {
+                    wx.showToast({
+                        title: '请输入正确的手机号码',
+                        icon: 'none'
+                    })
+                    return false;
+                }
+                var param = {
+                    formData: {
+                        cellphone: this.phone,
+                        verification_code: this.code
+                    }
+                }
+                api.setMemberPhone(param, this.globalData.member_id).then(da)
+            }
+        },
+        watch: {},
+        computed: {}
+    }
 </script>
 
 <style scoped>
-	.loginbox {
-		width: 540rpx;
-		margin: 0 auto;
-		padding-top: 40px;
-	}
+  .loginbox {
+    width: 540 rpx;
+    margin: 0 auto;
+    padding-top: 40px;
+  }
 
-	.loginbox .logo,
-	.loginbox .logo img {
-		display: block;
-		width: 175px;
-		margin: 0 auto;
-	}
+  .loginbox .logo,
+  .loginbox .logo img {
+    display: block;
+    width: 175px;
+    margin: 0 auto;
+  }
 
-	.loginbox form {
-		margin-top: 50px;
-	}
+  .loginbox form {
+    margin-top: 50px;
+  }
 
-	.loginbox .group {
-		height: 40px;
-		border-radius: 5px;
-		border: 1px solid #ff5454;
-		background: #fff;
-		font-size: 30rpx;
-	}
+  .loginbox .group {
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid #ff5454;
+    background: #fff;
+    font-size: 30 rpx;
+  }
 
-	.loginbox .group i {
-		display: block;
-		height: 20px;
-		width: 36px;
-	}
+  .loginbox .group i {
+    display: block;
+    height: 20px;
+    width: 36px;
+  }
 
-	.loginbox .group i image {
-		display: block;
-		width: 100%;
-		height: 100%;
-	}
+  .loginbox .group i image {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
 
-	.mt20 {
-		margin-top: 40rpx;
-	}
+  .mt20 {
+    margin-top: 40 rpx;
+  }
 
-	.loginbox .group input {
-		background: none;
-		border: none;
-		width: 100%;
-		height: 100%;
-	}
+  .loginbox .group input {
+    background: none;
+    border: none;
+    width: 100%;
+    height: 100%;
+  }
 
-	.loginbox .codebox>div {
-		width: 55%;
-		float: left;
-	}
+  .loginbox .codebox > div {
+    width: 55%;
+    float: left;
+  }
 
-	.loginbox .codebox>button {
-		float: right;
-		width: 40%;
-		background: #ff5454;
-		line-height: 40px;
-		color: #fff;
-		border-radius: 5px;
-		text-align: center;
-		font-size: 30rpx;
-		opacity: .8;
-	}
+  .loginbox .codebox > button {
+    float: right;
+    width: 40%;
+    background: #ff5454;
+    line-height: 40px;
+    color: #fff;
+    border-radius: 5px;
+    text-align: center;
+    font-size: 30 rpx;
+    opacity: .8;
+  }
 
-	.loginbox .codebox>button.active {
-		opacity: 1;
-	}
+  .loginbox .codebox > button.active {
+    opacity: 1;
+  }
 
-	.loginbox .submitBtn {
-		display: block;
-		width: 100%;
-		background: #ff5454;
-		color: #fff;
-		line-height: 80rpx;
-		height: 80rpx;
-		text-align: center;
-		border-radius: 10rpx;
-		border: none;
-		margin-top: 70rpx;
-	}
+  .loginbox .submitBtn {
+    display: block;
+    width: 100%;
+    background: #ff5454;
+    color: #fff;
+    line-height: 80 rpx;
+    height: 80 rpx;
+    text-align: center;
+    border-radius: 10 rpx;
+    border: none;
+    margin-top: 70 rpx;
+  }
 </style>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,111 +1,144 @@
 import $http from './request'
+
 /*var qs = require('qs');
 fly.post('/foo', qs.stringify({ 'bar': 123 }));*/
 export function JsontoForm(params) {
-	let str = [];
-	for (let key in params) {
-		str.push(key + "=" + params[key])
-	}
-	return str.join('&');
+  let str = [];
+  for (let key in params) {
+    str.push(key + "=" + params[key])
+  }
+  return str.join('&');
 
 }
+
 export function getmsglist() {
-	return $http.get({
-		url: '/content/announcement/get'
-	})
+  return $http.get({
+    url: '/content/announcement/get'
+  })
 }
+
 export function getbannerlist() {
-	return $http.get({
-		url: '/content/carousel/get'
-	})
+  return $http.get({
+    url: '/content/carousel/get'
+  })
 }
+
 export function getbaikelist(data) {
-	let params = JsontoForm(data);
-	return $http.get({
-		url: '/content/article/baike/get?' + params,
+  let params = JsontoForm(data);
+  return $http.get({
+    url: '/content/article/baike/get?' + params,
 
-	})
+  })
 }
+
 export function getjiankanglist(data) {
-	let params = JsontoForm(data);
-	return $http.get({
-		url: '/content/article/food/get?' + params,
-	})
+  let params = JsontoForm(data);
+  return $http.get({
+    url: '/content/article/food/get?' + params,
+  })
 }
-export function getbaike(id) {
-	return $http.get({
-		url: '/content/article/baike/get/' + id,
 
-	})
+export function getbaike(id) {
+  return $http.get({
+    url: '/content/article/baike/get/' + id,
+
+  })
 }
+
 export function getjiankang(id) {
-	return $http.get({
-		url: '/content/article/food/get/' + id,
-	})
+  return $http.get({
+    url: '/content/article/food/get/' + id,
+  })
 }
+
 export function getwechatlist(data) {
-	let params = JsontoForm(data);
-	return $http.get({
-		url: '/content/wechat_zoom/get?' + params,
-	})
+  let params = JsontoForm(data);
+  return $http.get({
+    url: '/content/wechat_zoom/get?' + params,
+  })
 }
+
 export function getOpenId(code) {
-	return $http.get({
-		url: '/api/firm/token/wechat/getOpenId?code=' + code,
-	})
+  return $http.get({
+    url: '/api/firm/token/wechat/getOpenId?code=' + code,
+  })
 }
+
 export function getMember(open_id) {
-	let login_code=wx.getStorageSync('login_code');
-	return $http.get({
-		url: '/api/firm/member/index?open_id=' + open_id+'&login_code='+login_code,
-	})
+  let login_code = wx.getStorageSync('login_code');
+  return $http.get({
+    url: '/api/firm/member/index?open_id=' + open_id + '&login_code=' + login_code,
+  })
 }
+
 export function addMember(param) {
-	let login_code=wx.getStorageSync('login_code');
-	return $http.post({
-		url: '/api/firm/member/add/one?login_code='+login_code,
-		data: param
-	})
+  let login_code = wx.getStorageSync('login_code');
+  return $http.post({
+    url: '/api/firm/member/add/one?login_code=' + login_code,
+    data: param
+  })
 }
+
 export function getMemberAddr(memberId) {
-	let login_code=wx.getStorageSync('login_code');
-	return $http.get({
-		url: '/api/firm/member_address/get/'+memberId+'?login_code='+login_code,
-	})
+  let login_code = wx.getStorageSync('login_code');
+  return $http.get({
+    url: '/api/firm/member_address/get/' + memberId + '?login_code=' + login_code,
+  })
 }
+
 export function addAddress(param) {
-	let login_code=wx.getStorageSync('login_code');
-	return $http.post({
-		url: '/api/firm/member_address/add/one?login_code='+login_code,
-		data: param
-	})
+  let login_code = wx.getStorageSync('login_code');
+  return $http.post({
+    url: '/api/firm/member_address/add/one?login_code=' + login_code,
+    data: param
+  })
 }
-export function editAddress(param,id) {
-	let login_code=wx.getStorageSync('login_code');
-	return $http.post({
-		url: '/api/firm/member_address/update/one/'+id+'?login_code='+login_code,
-		data: param
-	})
+
+export function editAddress(param, id) {
+  let login_code = wx.getStorageSync('login_code');
+  return $http.post({
+    url: '/api/firm/member_address/update/one/' + id + '?login_code=' + login_code,
+    data: param
+  })
 }
+
 export function delAddress(id) {
-	let login_code=wx.getStorageSync('login_code');
-	return $http.get({
-		url: '/api/firm/member_address/delete/one/'+id+'?login_code='+login_code,
-	})
+  let login_code = wx.getStorageSync('login_code');
+  return $http.get({
+    url: '/api/firm/member_address/delete/one/' + id + '?login_code=' + login_code,
+  })
 }
+
+export function sendVerifyCode(cellphone) {
+  let login_code = wx.getStorageSync('login_code');
+  return $http.get({
+    url: '/api/firm/member/getVerificationCode?cellphone=' + cellphone + '&login_code=' + login_code,
+  })
+}
+
+export function setMemberPhone(param, memberId) {
+  let login_code = wx.getStorageSync('login_code');
+  return $http.post({
+    url: '/api/firm/member/update/phone/' + memberId + '?login_code=' + login_code,
+    data: param
+  })
+}
+
 export default {
-	getmsglist,
-	getbannerlist,
-	getbaikelist,
-	getjiankanglist,
-	getwechatlist,
-	getbaike,
-	getjiankang,
-	getOpenId,
-	getMember,
-	addMember,
-	getMemberAddr,
-	addAddress,
-	editAddress,
-	delAddress
+  getmsglist,
+  getbannerlist,
+  getbaikelist,
+  getjiankanglist,
+  getwechatlist,
+  getbaike,
+  getjiankang,
+  getOpenId,
+  getMember,
+  addMember,
+  getMemberAddr,
+  addAddress,
+  editAddress,
+  delAddress,
+  sendVerifyCode,
+  setMemberPhone
 }
